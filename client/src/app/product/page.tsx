b4e8deddd422cc245b07eedb39d8b4f96127866a
Dave <t.piyabud@gmail.com>
2023-09-15 23:50:31 +0700

    feat: set project / tailwind basic

---
+++ client/src/app/product/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SingerProductPage() {
+  return (
+    <div>SingerProductPage</div>
+  )
+}
+
+export default SingerProductPage
---
+++ client/src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Notification() {
+  return (
+    <div className='h-12 bg-red-500 text-white flex justify-center items-center text-sm md:text-base cursor-pointer'>Free delivery for all orders over $50. Order your food now!</div>
+  )
+}
+
+export default Notification
