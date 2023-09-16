6d5c6577e16509c6da73ca104cdef8a42dede01b
Dave <t.piyabud@gmail.com>
2023-09-17 00:22:54 +0700

    feat : create responsive dropdown Menu

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Menu from "./Menu";
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="h-12 text-red-500 p-4 flex justify-between items-center border-b-2  border-b-red-500 uppercase">
+      {/* LoGO */}
+      <div>
+        <Link href="/">Pizza Heart</Link>
+      </div>
+      {/* Mobile */}
+      <div>
+        <Menu />
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- client/src/components/Notification.tsx
+++ client/src/components/Notification.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
 
 function Notification() {
   return (
-    <div className='h-12 bg-red-500 text-white flex justify-center items-center text-sm md:text-base cursor-pointer'>Free delivery for all orders over $50. Order your food now!</div>
-  )
+    <div  
+      className="
+      h-12 
+      bg-red-500 text-white text-sm
+      flex justify-center items-center 
+      md:text-base 
+      cursor-pointer"
+    >
+      Free delivery for all orders over $50. Order your food now!
+    </div>
+  );
 }
 
-export default Notification
+export default Notification;
