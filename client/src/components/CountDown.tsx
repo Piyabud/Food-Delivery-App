1b2b071f23debad10ddc69cba20637e1c43caeb4
piyabud <T_piyabud@outlook.com>
2023-10-06 02:14:10 +0700

    feat : add countdown

--- client/src/components/CountDown.tsx
+++ client/src/components/CountDown.tsx
@@ -2,12 +2,15 @@
 import React from "react";
 import Countdown from "react-countdown";
 
-const endingDate = new Date();
+const endingDate = new Date("2023-12-31T23:59:59");
 
 function CountDown() {
   return (
     <div>
-      <Countdown date={endingDate} className="font-bold text-5xl text-yellow-300"/>
+      <Countdown
+        date={endingDate}
+        className="font-bold text-5xl text-yellow-300"
+      />
     </div>
   );
 }
