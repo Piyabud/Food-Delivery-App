0efc880b3c0ee2ff34c84afcb7cda4a2a0997458
piyabud <T_piyabud@outlook.com>
2023-10-06 00:58:02 +0700

    fix : performant page

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -7,8 +7,8 @@ import CartIcon from "./CartIcon";
 const links = [
   { id: 1, title: "Homepage", url: "/" },
   { id: 2, title: "Menu", url: "/menu" },
-  { id: 3, title: "Working Hours", url: "/" },
-  { id: 4, title: "Contact", url: "/" },
+
+  { id: 3, title: "Contact", url: "/" },
 ];
 
 function Menu() {
