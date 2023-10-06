cda20b455134b360be792a39005738020c37048b
piyabud <T_piyabud@outlook.com>
2023-10-06 19:26:58 +0700

    feat : del border

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -74,7 +74,7 @@ const CartPage = () => {
         </div>
       </div>
       {/* PAYMENT CONTAINER */}
-      <div className="border border-green-500 h-1/3 p-4 bg-[--secondary] flex flex-col gap-4 justify-center lg:h-full lg:w-1/2 2xl:w-1/2 lg:px-20  2xl:text-xl 2xl:gap-6">
+      <div className="h-1/3 p-4 bg-[--secondary] flex flex-col gap-4 justify-center lg:h-full lg:w-1/2 2xl:w-1/2 lg:px-20  2xl:text-xl 2xl:gap-6">
         <div className="flex justify-between ">
           <span className="">Subtotal (3 items)</span>
           <span className="">$81.70</span>
